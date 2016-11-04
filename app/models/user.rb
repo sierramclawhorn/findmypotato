ae17ac29dbd3ce9cc4b41c5489665b69b3de1313
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-11-03 20:44:06 -0700

    Add tables to migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ActiveRecord::Base
 
   validates_presence_of :username
   validates_uniqueness_of :username
-  validates_presence_of :password
   validates_presence_of :email
   validates_uniqueness_of :email
+  validates_presence_of :password
   
   def password
     @password ||= Password.new(password_hash)
--- db/migrate/20161103203034_create_pins.rb
+++ db/migrate/20161103203034_create_pins.rb
@@ -1,4 +1,10 @@
 class CreatePins < ActiveRecord::Migration
   def change
+    create_table :pins do |t|
+      t.string :locations #check to see what kind of obj API is sending back!! (might not be a string)
+      t.integer :user_id
+
+      t.timestamps
+    end
   end
 end
