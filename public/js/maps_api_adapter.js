99e77946d19d00a234ea741cfc967d3fa47b854b
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-11-10 10:37:48 -0800

    Link js to html for map

--- public/js/maps_api_adapter.js
+++ public/js/maps_api_adapter.js
@@ -1,14 +1,14 @@
 function initMap() {
-  var myLatLng = {lat: -25.363, lng: 131.044};
+  var myLatLng = {lat: 37.7749, lng: -122.4312};
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 8,
     center: myLatLng
   });
 
   var marker = new google.maps.Marker({
     position: myLatLng,
     map: map,
-    title: 'Hello World!'
+    title: 'Potato Spotted!'
   });
 }
