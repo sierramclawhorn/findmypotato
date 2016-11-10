ba40ea183046ee1be605f8f390604735c387c8fe
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-11-10 10:45:25 -0800

    Animated markers on map

--- public/js/maps_api_adapter.js
+++ public/js/maps_api_adapter.js
@@ -1,14 +1,26 @@
+var marker;
+
 function initMap() {
-  var myLatLng = {lat: 37.7749, lng: -122.4312};
+  var myLatLng = {lat: 37.7949, lng: -122.3700};
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
+    zoom: 11,
     center: myLatLng
   });
 
-  var marker = new google.maps.Marker({
-    position: myLatLng,
+  marker = new google.maps.Marker({
     map: map,
-    title: 'Potato Spotted!'
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: {lat: 37.7840, lng: -122.4330}
   });
+  marker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
