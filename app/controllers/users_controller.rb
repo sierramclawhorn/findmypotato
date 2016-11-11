321ea82a62d968130efaca99b4a9a563238e896e
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-11-10 18:44:18 -0800

    Delete unused routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,3 @@
-# # USERS INDEX
-# get '/users' do
-#   @users = User.all
-#   erb :'users/index'
-# end
-
 # USERS NEW
 get '/users/new' do
   @user = User.new
@@ -28,31 +22,3 @@ post '/users' do
     erb :'users/new'
   end
 end
-
-# # USERS SHOW
-# get '/users/:id' do
-#   @user = User.find(params[:id])
-#   erb :'users/show'
-# end
-
-# # USERS EDIT
-# get '/users/:id/edit' do
-#   @user = User.find(params[:id])
-#   erb :'users/edit'
-# end
-
-
-# # USERS UPDATE
-# put '/users/:id' do
-#   @user = User.find(params[:id])
-#   @user.update(params[:user])
-#   redirect "/users/#{@user.id}"
-# end
-
-
-# # USERS DESTROY
-# delete '/users/:id' do
-#   @user = User.find(params[:id])
-#   @user.destroy
-#   redirect "/users"
-# end
